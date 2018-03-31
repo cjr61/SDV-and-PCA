b16cc5986c5a0c59e5bfb1b5004bd6a9d5ea9674
cjr61 <cjr61@zips.uakron.edu>
2018-03-30 20:11:40 -0400

    added ifs for the 4 functions

--- JavaApplication3/src/SVDPCA/SVDPCA.java
+++ JavaApplication3/src/SVDPCA/SVDPCA.java
@@ -1,6 +1,7 @@
 /*
  * SVD libraries found from https://math.nist.gov/javanumerics/jama/ and inludes the files:
- * CholeskyDecomposition, EigenvalueDecomposition, LUDecomposition, Maths, Matrix, QRDecomposition, and SingularValueDecomposition
+ * CholeskyDecomposition, EigenvalueDecomposition, LUDecomposition, Maths, Matrix, QRDecomposition, 
+ * and SingularValueDecomposition
  */
 package SVDPCA;
 
@@ -14,7 +15,22 @@ public class SVDPCA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        if (args[1] == "1"){
+        	//Int to Binary Conversion
+        		
+        }
+        else if (args[2] == "2") {
+        	//Binary to Int conversion
+        }
+        else if (args[3] == "3") {
+        	//SVD 
+        }
+        else if (args[4] == "4") {
+        	//Image reconstruction 
+        }
+        else {
+        	System.out.println("Incorrect input parameters");
+        }
     }
     
 }
