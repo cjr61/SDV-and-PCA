7e92a02ca9576f7028d1eafa71f17db4e09d708d
cjr61 <cjr61@zips.uakron.edu>
2018-03-30 21:13:16 -0400

    Added some Binary work

--- JavaApplication3/src/SVDPCA/BinaryOps.java
+++ JavaApplication3/src/SVDPCA/BinaryOps.java
@@ -1,7 +1,26 @@
 package SVDPCA;
 
+import java.io.*;
 import java.util.*;
 
 public class BinaryOps {
 	
+	public Vector<Integer> ReadFile(String userFileName) throws FileNotFoundException {
+	    File infile = new File("../../../"+ userFileName); //images should be placed at the root of the project
+	    Scanner scan = new Scanner(infile);
+	    Vector<Integer> fileInfo = new Vector<Integer>();
+	    while(scan.hasNextInt()) {
+	    	fileInfo.add(scan.nextInt());
+	    }
+	    scan.close();
+	    return fileInfo;
+	}
+	
+	public void IntFileToBinary(String userFileName) {
+		
+	}
+	
+	public void BinaryFileToInt(String userFileName) {
+		
+	}
 }
